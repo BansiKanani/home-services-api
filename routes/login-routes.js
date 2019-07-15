96b20dfa48d5129bb3212b36a5b088a36d8824bb
BansiKanani <bansi.kumar.kanani@gmail.com>
2019-07-15 10:24:15 +0530

    allowed res.header Access-Control-Allow-Origin

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -5,6 +5,7 @@ function routes(Login) {
   router
     .route('/')
     .get((req, res) => {
+      res.header("Access-Control-Allow-Origin", "*");
       Login.findOne(req.body, (err, login) => {
         if (err) return res.send(err);
         if (login != null) return res.json({ user: login.user });
@@ -12,6 +13,7 @@ function routes(Login) {
       });
     })
     .post((req, res) => {
+      res.header("Access-Control-Allow-Origin", "*");
       const login = new Login(req.body);
       login.save();
       return res.status(201).json(login);
